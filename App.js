cce001c72f0debddde26961f75d5e9e0b6db2dd9
Daexo-coder <lichabamonehela@gmail.com>
2024-03-22 16:16:04 +0200

    prospectus

--- App.js
+++ App.js
@@ -1,48 +1,190 @@
-import React from "react-native";
+import React from "react-native"
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, ScrollView} from "react-native";
+import { StyleSheet, Text, View, Image, TouchableOpacity} from "react-native"
+import { AntDesign } from '@expo/vector-icons';
 
 export default function App() {
+let rating = 1;
+let setRating;
+function increaseRate(){
+    setRating = rating + 1;
+    return setRating;
+}
+function DecreaseRate(){
+    setRating=rating - 1;
+    return setRating;
+}
 
 
   return (
     <View style={styles.container}>
-      <View style ={styles.innerView}>
-      <View style={styles.atHeader}>
-        <View style ={styles.profileView}>
-        <Image source={require("./mySelf.avif")} style={styles.profilePhoto} />
+      <View style={styles.innerView}>
+        <View style={styles.schoolLabel}>
+          <Image source={require("./Limkokwing.jpg")} style={styles.schoolLogo}/>
         </View>
-      </View>
-       <View style={styles.main}>
-          <ScrollView style={styles.profileDescription}>
-          <View style={styles.profileIdentity}>
-          <Text style={styles.Req}>Name: <Text style={styles.name}>Monehela Lichaba</Text></Text>
-            <Text style={styles.Req}>Student ID: <Text style={styles.studentID}>901016759</Text></Text>
-          </View>
-            <View style={styles.headers}>
-              <Text style={styles.Tableheaders}>course</Text>
-              <Text style={styles.Tableheaders}>Credit</Text>
-            </View>
-            <View style={styles.tableData}>
-              <View style={styles.forCourse}>
-                <Text style={styles.data}>Calculus</Text>
-                <Text style={styles.data}>C++ 1</Text>
-                <Text style={styles.data}>Java 1</Text>
-                <Text style={styles.data}>Algebra</Text>
-                <Text style={styles.data}>Database</Text>
+            <View style={styles.courseContainer}>
+              <View style={styles.descriptionPanel}>
+                <Text style={styles.header1}>
+                  Fashion textiles
+                </Text>
+                <Text style={styles.courseDetails}>
+                  Fashion and textiles is the primary culture of every nation,
+                  hence we need very creative designers of clothes, Limkowing 
+                  offers you the best of this.
+                </Text>
+                <View style={styles.requirements}>
+                  <Text style={styles.CR}>1. minimum of D credit in math</Text>
+                  <Text style={styles.CR}>2. minimum of D credit in English</Text>
+                  <Text style={styles.CR}>3. minimum of C  credit in design subjects</Text>
+                  <Text style={styles.CR}>4. minimum of E credit in all other subjects</Text>
+                </View>
+                <View style={styles.rateCourse}>
+                  <Text style={styles.rate}>Rate course out of 6   </Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={DecreaseRate()}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  <Text style={styles.RR}>{setRating}</Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={increaseRate()}>
+                  <AntDesign name="pluscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  
+                </View>
               </View>
-              <View style={styles.forCredit}>
-                <Text style={styles.data}>B*</Text>
-                <Text style={styles.data}>A*</Text>
-                <Text style={styles.data}>B-</Text>
-                <Text style={styles.data}>B</Text>
-                <Text style={styles.data}>D</Text>
+                <View style={styles.photoPanel}>
+                <Image source={require("./fashion.jpg")} style={styles.imageStyles}/>
+                </View>
               </View>
-            </View>
-          </ScrollView>
-        </View>
+            <View style={styles.courseContainer}>
+              <View style={styles.descriptionPanel}>
+                <Text style={styles.header1}>
+                 Tourism management
+                </Text>
+                <Text style={styles.courseDetails}>
+                 Tourism management is the force of attraction in every country,
+                  hence one source of countries revevue, Limkokwing offers you 
+                  the best af this.
+                </Text>
+                <View style={styles.requirements}>
+                  <Text style={styles.CR}>1. minimum of D credit in math</Text>
+                  <Text style={styles.CR}>2. minimum of C credit in English</Text>
+                  <Text style={styles.CR}>3. minimum of C  credit in design subjects</Text>
+                  <Text style={styles.CR}>4. minimum of E credit in all other subjects</Text>
+                </View>
+                <View style={styles.rateCourse}>
+                  <Text style={styles.rate}>Rate course out of 6   </Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={DecreaseRate()}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  <Text style={styles.RR}>{setRating}</Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={increaseRate()}>
+                  <AntDesign name="pluscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  
+                </View>
+              </View>
+                <View style={styles.photoPanel}>
+                <Image source={require("./tourism.jpg")} style={styles.imageStyles}/>
+                </View>
+              </View>
+            <View style={styles.courseContainer}>
+              <View style={styles.descriptionPanel}>
+                <Text style={styles.header1}>
+                  film Production
+                </Text>
+                <Text style={styles.courseDetails}>
+                  Art as a whole is the reason for entertainment, from acient times
+                  art has played a remarmakeble part in making people happy, Limkokwing
+                  offers you that happiness.
+                </Text>
+                <View style={styles.requirements}>
+                  <Text style={styles.CR}>1. minimum of C credit in math</Text>
+                  <Text style={styles.CR}>2. minimum of C credit in English</Text>
+                  <Text style={styles.CR}>3. minimum of C  credit in design subjects</Text>
+                  <Text style={styles.CR}>4. minimum of C credit in all other subjects</Text>
+                </View>
+                <View style={styles.rateCourse}>
+                  <Text style={styles.rate}>Rate course out of 6   </Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={DecreaseRate()}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  <Text style={styles.RR}>{setRating}</Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={increaseRate()}>
+                  <AntDesign name="pluscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  
+                </View>
+              </View>
+                <View style={styles.photoPanel}>
+                <Image source={require("./filmProduction.jpg")} style={styles.imageStyles}/>
+                </View>
+              </View>
+            <View style={styles.courseContainer}>
+              <View style={styles.descriptionPanel}>
+                <Text style={styles.header1}>
+                  fashion and textiles
+                </Text>
+                <Text style={styles.courseDetails}>
+                  As part of art, photography can communicate with us in many
+                  aspect of our life, it give life to every object it induldge in.
+                  Limkokwing offers you the best of this.
+                </Text>
+                <View style={styles.requirements}>
+                  <Text style={styles.CR}>1. minimum of C credit in math</Text>
+                  <Text style={styles.CR}>2. minimum of C credit in English</Text>
+                  <Text style={styles.CR}>3. minimum of C  credit in design subjects</Text>
+                  <Text style={styles.CR}>4. minimum of D credit in all other subjects</Text>
+                </View>
+                <View style={styles.rateCourse}>
+                  <Text style={styles.rate}>Rate course out of 6   </Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={DecreaseRate()}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  <Text style={styles.RR}>{setRating}</Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={increaseRate()}>
+                  <AntDesign name="pluscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  
+                </View>
+              </View>
+                <View style={styles.photoPanel}>
+                <Image source={require("./graphicDesign.jpg")} style={styles.imageStyles}/>
+                </View>
+              </View>
+            <View style={styles.courseContainer}>
+              <View style={styles.descriptionPanel}>
+                <Text style={styles.header1}>
+                  Information Technology
+                </Text>
+                <Text style={styles.courseDetails}>
+                  Technology is the core source advancement of every country, 
+                  since weare in the fourth industrial revolution, we need technology 
+                  now more than ever.
+                </Text>
+                <View style={styles.requirements}>
+                  <Text style={styles.CR}>1. minimum of C credit in math</Text>
+                  <Text style={styles.CR}>2. minimum of C credit in English</Text>
+                  <Text style={styles.CR}>3. minimum of C  credit in design subjects</Text>
+                  <Text style={styles.CR}>4. minimum of C credit in all other subjects</Text>
+                </View>
+                <View style={styles.rateCourse}>
+                  <Text style={styles.rate}>Rate course out of 6   </Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={DecreaseRate()}>
+                  <AntDesign name="minuscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  <Text style={styles.RR}>{setRating}</Text>
+                  <TouchableOpacity style={styles.Ibuttom} onPress={increaseRate()}>
+                  <AntDesign name="pluscircleo" size={24} color="black" />
+                  </TouchableOpacity>
+                  
+                </View>
+              </View>
+                <View style={styles.photoPanel}>
+                <Image source={require("./IT.jpg")} style={styles.imageStyles}/>
+                </View>
+              </View>
       </View>
-        <StatusBar style="auto" />
+      <StatusBar style="auto" />
+      
     </View>
   )
 };
@@ -50,95 +192,85 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#F0F8FF",
+    backgroundColor: "#999999",
     alignItems: "center",
     justifyContent:"center",
-      
   },
-
+  RR:{
+    marginLeft:"2.2em",
+    fontSize:"1.4em",
+    fontWeight:"800",
+  },
   innerView:{
-    width: "50vh",
-    height: "80%",
-    backgroundColor: "white",
-    borderRadius: ".8em",
-    borderWidth:".01em",
-    borderColor:"#2f4f4f",
-    padding:".4em",
-  },
-  atHeader:{
-    width: "100%",
-    height: " 22%",
-    backgroundColor:"#cccccc",
-  },
-  profilePhoto:{
-    borderRadius: "100%",
-    height: "100%",
-    width: "100%",
-  },
-  profileView:{
-    height: "100%",
-    width:" 35%",
-  },
-  profileIdentity:{
-    backgroundColor:"black",
-    width: "100%",
-    borderWidth: ".02em",
-    borderBlockColor:" white",
-    borderWidth: "#2f4f4f",
-  },
-  Req:{
-    color:"white",
-  },
-  profileDescription:{
-    width: "100%",
-    height:"40%",
-    backgroundColor:"black",
-    position:"absolute",
-    borderBlockColor: "#2f4f4f",
-    borderWidth:".01em",
-    borderRadius:".2em",
-  },
-  main:{
-    width:" 100%",
-    marginTop:"2%",
-    height: " 75%",
-    backgroundColor:"#cccccc",
-  },
-
-  headers:{
-    width: "100%",
-    flexDirection: "row",
-    justifyContent:"space-around",
-    backgroundColor: "#A9A9A9",
-    padding:0,
-  },
-
-  tableData:{
-    flexDirection: "row",
-    width:"100%",
-    justifyContent:"space-between",
-    backgroundColor:"white",
+    backgroundColor:"rgba(234, 242, 249, 0.651)",
+    height:"90vh",
+    width:"60%",
     padding:".5em",
-    height: "auto",
-  },
-  Tableheaders:{
-    fontSize:".7em",
-    textTransform: "uppercase",
-    color: " #8a8a8a",
-    margin: 0,
-    fontWeight:"bold",
+    overflow:"scroll",
+    overflowX:"hidden",
     
+  
   },
-  name:{
+  courseContainer:{
+    backgroundColor:"black",
+    width:"100%",
+    height:"35%",
+    flexDirection:"row",
+    justifyContent:"none",
+    marginBottom:".3em"
+  },
+  descriptionPanel:{
+    backgroundColor:"white",
+    width:"50%",
+    height:"100%",
+  },
+  photoPanel:{
+    backgroundColor:"green",
+    width:"50%",
+    height:"100%",
+  },
+  imageStyles:{
+    width:"100%",
+    height:"100%",
+  },
+
+  header1:{
+    backgroundColor:"black",
+    color:"white",
+    textTransform:"uppercase",
+    textAlign:"center",
+   
+  },
+  courseDetails:{
+    padding:".5em",
+    fontWeight:"500",
+  },
+  requirements:{
+    padding:".5em",
     fontStyle:"italic",
-    textTransform: " uppercase",
-    fontWeight:"bold",
-    fontSize:".8em",
-    color: "white",
   },
-  studentID:{
-    fontStyle:"italic",
-    fontSize:".9em",
-    color: "white",
+  schoolLabel:{
+    height:"20%",
+    width:"100%",
+    marginBottom:"1em",
   },
+  schoolLogo:{
+    height:"100%",
+    width:"100%",
+    objectFit:"fill",
+  },
+  rateCourse:{
+    width:"100%",
+    height:"15%",
+    borderTopWidth:".1em",
+    borderColor:"#8a8a8a",
+    borderBottomWidth:".1em",
+    flexDirection:"row",
+  },
+  Ibuttom:{
+    marginLeft:"3em",
+    size:"1em",
+  }
+
+
 });
